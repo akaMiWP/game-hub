948d58baa96eab35b1fe793200925885b5149c82
akaMiWP <bangkokappdevelopment@gmail.com>
2024-06-30 13:03:32 +0700

    Built a navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import logo from "../assets/logo.webp";
+import { HStack, Image, Text } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px"></Image>
+      <Text>NavBar</Text>
+    </HStack>
+  );
+};
+
+export default Navbar;
